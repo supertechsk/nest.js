514dc3b7aaed49ad94d708a4e963b6cc52060ac3
supertechsk <sachinkumar1852sk@gmail.com>
2025-01-14 21:16:01 +0530

    Update nest.js

--- nest.js
+++ nest.js
@@ -1,1 +1,22 @@
 // This is nest js page
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+
+@Module({
+  controllers: [AppController],
+})
+export class AppModule {}
+
+
+
+
+import { Controller, Get } from '@nestjs/common';
+
+@Controller()
+export class AppController {
+  @Get()
+  getHello(): string {
+    return 'Hello, World!';
+  }
+}
+
