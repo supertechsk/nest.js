ecb9e231fe1fe4e3574dc063b373fbaafdb3d95c
supertechsk <sachinkumar1852sk@gmail.com>
2025-01-30 14:11:19 +0530

    Update nest.js

--- nest.js
+++ nest.js
@@ -17,7 +17,7 @@ import { Controller, Get } from '@nestjs/common';
 export class AppController {
   @Get()
   getHello(): string {
-    return 'Hello, nest.js!';
+    return 'Hello, nest.js';
   }
 }
 
